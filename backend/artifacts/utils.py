59173d7f40d111ea449590147209f85e6263f050
Bekim <bekim.rexh@gmail.com>
2025-08-16 12:22:23 +0100

    first commit

---
+++ backend/artifacts/utils.py
@@ -0,0 +1,22 @@
+import json
+
+__data_columns = None
+__model = None
+
+
+def get_marital_status():
+
+
+def get_job_types():
+    
+
+
+
+def load_artifacts():
+    #load model
+    with open(something as f):
+        __data_columns = json.load(f)
+
+    #load the columns
+    with open()
+
